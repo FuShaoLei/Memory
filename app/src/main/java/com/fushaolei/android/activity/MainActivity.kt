ba3ce1be06e38aef0f067b3b5a0cb572ec1130b4
FuShaoLei <1563250958@qq.com>
2022-10-29 20:36:22 +0800

    feat: 打开照相机和相册选择图片

--- app/src/main/java/com/fushaolei/android/activity/MainActivity.kt
+++ app/src/main/java/com/fushaolei/android/activity/MainActivity.kt
@@ -1,19 +1,100 @@
 package com.fushaolei.android.activity
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import com.fushaolei.android.R
 import com.fushaolei.android.base.BaseActivity
 import com.fushaolei.android.databinding.ActivityMainBinding
 import com.fushaolei.android.start
+import java.io.File
+
 
 class MainActivity : BaseActivity() {
     private lateinit var binding: ActivityMainBinding
+    val take_photo = 1
+    val select_album = 2
+    lateinit var imageUri: Uri
+    lateinit var outputImage: File
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.tvSetting.setOnClickListener { start<ConfigurationActivity>(this) }
 
+        binding.tvUpload.setOnClickListener {
+            selectImg()
+        }
     }
+
+    fun openAlbum() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "image/*"
+        startActivityForResult(intent, select_album)
+    }
+
+
+    private fun selectImg() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+        startActivityForResult(intent, select_album)
+    }
+
+    /**
+     * 打开照相机
+     */
+    fun openCamera() {
+        outputImage = File(externalCacheDir, "output_image.jpg")
+        if (outputImage.exists()) {
+            outputImage.delete()
+        }
+        outputImage.createNewFile()
+        imageUri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            FileProvider.getUriForFile(
+                this,
+                "com.fushaolei.android.activity.fileprovider",
+                outputImage
+            )
+        } else {
+            Uri.fromFile(outputImage)
+        }
+
+        val intent = Intent("android.media.action.IMAGE_CAPTURE")
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        startActivityForResult(intent, take_photo)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            take_photo -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    val bitmap =
+                        BitmapFactory.decodeStream(contentResolver.openInputStream(imageUri))
+                    binding.tvImg.setImageBitmap(bitmap)
+                }
+            }
+            select_album -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    data.data?.let { uri ->
+                        val bitmap = getBitmapFromUri(uri)
+                        binding.tvImg.setImageBitmap(bitmap)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getBitmapFromUri(uri: Uri) =
+        contentResolver.openFileDescriptor(uri, "r")?.use {
+            BitmapFactory.decodeFileDescriptor(it.fileDescriptor)
+        }
 }
